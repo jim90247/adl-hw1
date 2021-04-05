0041996e3c29dcb33d3257e6e13e6fa8ea7aff5b
jim90247 <jim90247@gmail.com>
2021-04-05 14:14:47 +0800

    Implement SeqLabeller

--- model.py
+++ model.py
@@ -3,21 +3,15 @@ from typing import Dict
 import torch
 import torch.nn as nn
 from torch.nn import Embedding
+from torch.nn.functional import log_softmax
 
 
-class SeqClassifier(torch.nn.Module):
+class SeqModel(torch.nn.Module):
     NET_TYPES = {'rnn': nn.RNN, 'lstm': nn.LSTM, 'gru': nn.GRU}
 
-    def __init__(self,
-                 embeddings: torch.tensor,
-                 padding_idx: int,
-                 hidden_size: int,
-                 num_layers: int,
-                 dropout: float,
-                 bidirectional: bool,
-                 num_class: int,
-                 net_type: str = 'lstm') -> None:
-        super(SeqClassifier, self).__init__()
+    def __init__(self, embeddings: torch.tensor, padding_idx: int, hidden_size: int, num_layers: int, dropout: float,
+                 bidirectional: bool, num_class: int, net_type: str) -> None:
+        super(SeqModel, self).__init__()
         self.embed = Embedding.from_pretrained(embeddings, freeze=False, padding_idx=padding_idx)
         # TODO: model architecture
 
@@ -43,6 +37,22 @@ class SeqClassifier(torch.nn.Module):
 
     def forward(self, batch) -> Dict[str, torch.Tensor]:
         # TODO: implement model forward
+        raise NotImplementedError
+
+
+class SeqClassifier(SeqModel):
+    def __init__(self,
+                 embeddings: torch.tensor,
+                 padding_idx: int,
+                 hidden_size: int,
+                 num_layers: int,
+                 dropout: float,
+                 bidirectional: bool,
+                 num_class: int,
+                 net_type: str = 'lstm') -> None:
+        super().__init__(embeddings, padding_idx, hidden_size, num_layers, dropout, bidirectional, num_class, net_type)
+
+    def forward(self, batch) -> Dict[str, torch.Tensor]:
         embedded = self.embed(batch)
 
         if self.net_type == 'lstm':
@@ -58,3 +68,29 @@ class SeqClassifier(torch.nn.Module):
         dropped = self.dropout(last_layer_hidden)
 
         return self.fc(dropped)
+
+
+class SeqLabeller(SeqModel):
+    def __init__(self,
+                 embeddings: torch.tensor,
+                 padding_idx: int,
+                 hidden_size: int,
+                 num_layers: int,
+                 dropout: float,
+                 bidirectional: bool,
+                 num_class: int,
+                 net_type: str = 'lstm') -> None:
+        super().__init__(embeddings, padding_idx, hidden_size, num_layers, dropout, bidirectional, num_class, net_type)
+
+    def forward(self, batch) -> Dict[str, torch.Tensor]:
+        embedded = self.embed(batch)
+
+        # output shape (batch, seq_len, num_directions * hidden_size) as batch_first is provided
+        output, _ = self.rnn(embedded)
+
+        output = output.view(-1, output.shape[2])
+
+        dropped = self.dropout(output)
+
+        # Using `log_softmax` here enables simpler implementation of custom loss function
+        return log_softmax(self.fc(dropped))
